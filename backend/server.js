739c3cf42d78c8f68aa549b91e744db4ce16d7f2
Stuti Yadav <b23ch1044@iitj.ac.in>
2026-05-27 11:02:42 +0530

    fix: add ping endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.get("/ping", (req, res) => res.send("pong"));
 
 const server = http.createServer(app);
 const io = new Server(server, {
